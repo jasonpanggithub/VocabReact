67c5db59faf3dd2e3f45dc5e2abad28dc79eb874
Jason Pang <jason.win8@hotmail.com>
2026-01-25 20:09:49 +0000

    remove About and Home

--- src/navigation/navItems.jsx
+++ src/navigation/navItems.jsx
@@ -1,13 +1,10 @@
-import Home from '../pages/Home/Home'
 import List from '../pages/List/List'
 import Add from '../pages/Add/Add'
 import ByDate from '../pages/Dictation/ByDate'
 import TNPlus from '../pages/Dictation/TNPlus'
 import Edit from '../pages/Edit/Edit'
-import About from '../pages/About/About'
 
 const navItems = [
-  { path: '/', label: 'Home', element: <Home /> },
   { path: '/list', label: 'List', element: <List /> },
   { path: '/add', label: 'Add', element: <Add /> },
   {
@@ -18,7 +15,6 @@ const navItems = [
     ],
   },
   { path: '/edit/:id', element: <Edit /> },  // though it is not shown in the menu, we keep it here for routing purposes
-  { path: '/about', label: 'About', element: <About /> },
 ]
 
 export default navItems
